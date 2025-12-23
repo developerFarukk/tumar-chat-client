e03e60a2c705d9a584dbca44e8ac5f80f698c401
FARUK2059 <omarfaruk34552@gmail.com>
2025-12-23 19:01:57 +0600

    Update layout and add login/signup pages for Amar Chat

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+const LoginPageServer = () => {
+
+    return (
+        <div>
+            <div> The Component is Start LoginPageServer </div>
+        </div>
+    );
+};
+
+export default LoginPageServer;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+const SignupPageServer = () => {
+
+    return (
+        <div>
+            <div> The Component is Start SignupPageServer </div>
+        </div>
+    );
+};
+
+export default SignupPageServer;
