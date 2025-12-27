91f3254e69f7d8bab657078f1ed15e8064fd3a89
FARUK2059 <omarfaruk34552@gmail.com>
2025-12-28 01:54:33 +0600

    feat: add ChatPageHome and ProfileHeader components with initial structure

---
+++ src/app/(chatPage)/chat/page.tsx
@@ -0,0 +1,15 @@
+import ChatPageHome from "@/components/module/chatPage/ChatPageHome";
+
+
+
+
+const ChatePageServer = () => {
+
+    return (
+        <div>
+            <ChatPageHome />
+        </div>
+    );
+};
+
+export default ChatePageServer;
