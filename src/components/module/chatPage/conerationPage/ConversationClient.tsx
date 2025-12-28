9a058b437dd6710177b64678f4e08959bc987e4a
FARUK2059 <omarfaruk34552@gmail.com>
2025-12-28 20:12:36 +0600

    feat: remove commented-out user profile section in ConversationClient

--- src/components/module/chatPage/conerationPage/ConversationClient.tsx
+++ src/components/module/chatPage/conerationPage/ConversationClient.tsx
@@ -34,26 +34,6 @@ const ConversationClient = () => {
         </div>
       </header>
       <footer className="w-full">
-        {/* <div className="border-2 p-2 rounded-lg">
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-2">
-              <div className="size-10">
-                <Image
-                  // src={selectedImg || authUser.profilePic || "/avatar.png"}
-                  src={userProfile}
-                  alt="User image"
-                  className="size-full object-cover"
-                />
-              </div>
-              <h2 className="font-semibold">Omar Faruk</h2>
-            </div>
-            <div>
-              <Link href="/chat">
-                <X />
-              </Link>
-            </div>
-          </div>
-        </div> */}
         <SendMessage />
       </footer>
     </div>
