9e4e378fbfdfaa6511be4d2596c4490a0a219d62
FARUK2059 <omarfaruk34552@gmail.com>
2025-12-28 16:06:47 +0600

    feat: update AppSidebar to improve user profile display in Chats and Contacts tabs

--- src/components/module/chatPage/chatDashboard/app-sidebar.tsx
+++ src/components/module/chatPage/chatDashboard/app-sidebar.tsx
@@ -216,46 +216,50 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             </TabsList>
           </div>
           <TabsContent value="chats">
-            <Link href="/chat">
-              <div className="border-2 p-1 rounded-lg">
-                <div className="flex justify-between items-center">
-                  <div className="flex items-center gap-2">
-                    <div className="size-10">
-                      <Image
-                        // src={selectedImg || authUser.profilePic || "/avatar.png"}
-                        src={userProfile}
-                        alt="User image"
-                        className="size-full object-cover"
-                      />
+            <div className="">
+              <div className="mt-1">
+                <Link href="/chat">
+                  <div className="border-2 p-1 rounded-lg">
+                    <div className="flex justify-between items-center">
+                      <div className="flex items-center gap-2">
+                        <div className="size-10">
+                          <Image
+                            // src={selectedImg || authUser.profilePic || "/avatar.png"}
+                            src={userProfile}
+                            alt="User image"
+                            className="size-full object-cover"
+                          />
+                        </div>
+                        <h2 className="font-semibold">Omar Faruk</h2>
+                      </div>
+                      <h2 className="">Ofline</h2>
                     </div>
-                    <h2 className="font-semibold">Omar Faruk</h2>
                   </div>
-                  <h2 className="">Ofline</h2>
-                </div>
+                </Link>
               </div>
-            </Link>
+            </div>
           </TabsContent>
           <TabsContent value="contacts">
             <div className="">
               <div className="mt-1">
                 <Link href="/chat">
-                <div className="border-2 p-1 rounded-lg">
-                  <div className="flex justify-between items-center">
-                    <div className="flex items-center gap-2">
-                      <div className="size-10">
-                        <Image
-                          // src={selectedImg || authUser.profilePic || "/avatar.png"}
-                          src={userProfile}
-                          alt="User image"
-                          className="size-full object-cover"
-                        />
+                  <div className="border-2 p-1 rounded-lg">
+                    <div className="flex justify-between items-center">
+                      <div className="flex items-center gap-2">
+                        <div className="size-10">
+                          <Image
+                            // src={selectedImg || authUser.profilePic || "/avatar.png"}
+                            src={userProfile}
+                            alt="User image"
+                            className="size-full object-cover"
+                          />
+                        </div>
+                        <h2 className="font-semibold">Omar Faruk</h2>
                       </div>
-                      <h2 className="font-semibold">Omar Faruk</h2>
+                      <h2 className="">Ofline</h2>
                     </div>
-                    <h2 className="">Ofline</h2>
                   </div>
-                </div>
-              </Link>
+                </Link>
               </div>
               <div className="mt-1">
                 <Link href="/chat">
