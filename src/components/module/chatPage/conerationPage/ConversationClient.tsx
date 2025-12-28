944cc5c159c86ae480eef80d9935445f622b901c
FARUK2059 <omarfaruk34552@gmail.com>
2025-12-29 00:36:18 +0600

    feat: update ConversationClient layout for improved structure and styling

--- src/components/module/chatPage/conerationPage/ConversationClient.tsx
+++ src/components/module/chatPage/conerationPage/ConversationClient.tsx
@@ -8,71 +8,54 @@ import SendMessage from "@/components/shared/SendMessage";
 
 const ConversationClient = () => {
   return (
-    // <div className="min-h-screen flex flex-col">
-    //   {/* header */}
-    //   <header className="bg-fuchsia-200">
-    //     <div className="border-2 p-2 rounded-lg">
-    //       <div className="flex justify-between items-center">
-    //         <div className="flex items-center gap-2">
-    //           <div className="size-10">
-    //             <Image
-    //               src={userProfile}
-    //               alt="User image"
-    //               className="size-full object-cover"
-    //             />
-    //           </div>
-    //           <h2 className="font-semibold">Omar Faruk</h2>
-    //         </div>
-
-    //         <Link href="/chat">
-    //           <X />
-    //         </Link>
-    //       </div>
-    //     </div>
-    //   </header>
-
-    //   {/* body */}
-    //   <div className="flex-1 overflow-auto px-3 py-2">
-    //     body text message show
-    //   </div>
-
-    //   {/* footer */}
-    //   <footer className="w-full mt-auto">
-    //     <SendMessage />
-    //   </footer>
-    // </div>
-    <div className=" flex flex-col">
-      {/* header */}
-      <header className="bg-fuchsia-200">
-        <div className="border-2 p-2 rounded-lg">
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-2">
-              <div className="size-10">
-                <Image
-                  src={userProfile}
-                  alt="User image"
-                  className="size-full object-cover"
-                />
+    <div className="border-2 rounded-xl p-2 bg-fuchsia-100">
+      <div className=" flex flex-col">
+        {/* header */}
+        <header className="bg-fuchsia-200">
+          <div className="border-2 p-2 rounded-lg">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-2">
+                <div className="size-10">
+                  <Image
+                    src={userProfile}
+                    alt="User image"
+                    className="size-full object-cover"
+                  />
+                </div>
+                <h2 className="font-semibold">Omar Faruk</h2>
               </div>
-              <h2 className="font-semibold">Omar Faruk</h2>
+
+              <Link href="/chat">
+                <X />
+              </Link>
+            </div>
+          </div>
+        </header>
+
+        {/* body */}
+        <div className="min-h-[70vh] bg-sky-100 m-2 p-2 rounded-lg">
+          <div>
+            {/* My text */}
+            <div className="flex justify-end mt-1">
+              <h2 className="text-lg bg-sky-300 w-fit rounded-md p-2">
+                this is my text
+              </h2>
             </div>
 
-            <Link href="/chat">
-              <X />
-            </Link>
+            {/*  friend  text */}
+            <div className="flex justify-start mt-1">
+              <h2 className="text-lg bg-amber-100 w-fit rounded-md p-2">
+                this is friend text
+              </h2>
+            </div>
           </div>
         </div>
-      </header>
 
-      {/* body */}
-      <div className="min-h-[72vh]">
-        body text message show
+        {/* footer */}
+        <footer className="w-full flex-none">
+          <SendMessage />
+        </footer>
       </div>
-
-      {/* footer */}
-      <footer className="w-full flex-none">
-        <SendMessage />
-      </footer>
     </div>
   );
 };
