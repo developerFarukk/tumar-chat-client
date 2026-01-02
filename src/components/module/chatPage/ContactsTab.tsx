4e4de2bf410a81988a700afe7a94413c19474db8
FARUK2059 <omarfaruk34552@gmail.com>
2026-01-02 17:52:57 +0600

    feat:  contacts data implement success

--- src/components/module/chatPage/ContactsTab.tsx
+++ src/components/module/chatPage/ContactsTab.tsx
@@ -1,4 +1,6 @@
-"use client"
+
+
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -6,56 +8,43 @@ import userProfile from "../../../../public/avatar.png";
 import { TUser } from "@/type/auth";
 
 export interface TContactsData {
-  allContacts: TUser[]
+  allContacts: TUser[];
 }
 
-const ContactsTab = ({allContacts}: TContactsData) => {
-
-  console.log(allContacts);
-  
+const ContactsTab = ({ allContacts }: TContactsData) => {
+  // console.log(allContacts?.length);
 
   return (
     <div className="">
-      <div className="mt-1">
-        <Link href="/chat">
-          <div className="border-2 p-1 rounded-lg">
-            <div className="flex justify-between items-center">
-              <div className="flex items-center gap-2">
-                <div className="size-10">
-                  <Image
-                    // src={selectedImg || authUser.profilePic || "/avatar.png"}
-                    src={userProfile}
-                    alt="User image"
-                    className="size-full object-cover"
-                  />
+      {allContacts?.length > 0 ? (
+        allContacts.map((contact: TUser) => (
+          <div key={contact?._id || contact?.email} className="mt-1">
+            <Link href="/chat">
+              <div className="border-2 p-1 rounded-lg">
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center gap-2">
+                    <div className="size-10">
+                      <Image
+                        src={ contact?.image || userProfile}
+                        alt="User image"
+                        className="size-full object-cover"
+                      />
+                    </div>
+
+                    <h2 className="font-semibold">
+                      {contact?.name || "Unknown User"}
+                    </h2>
+                  </div>
+
+                  <h2 className="">Offline</h2>
                 </div>
-                <h2 className="font-semibold">Omar Faruk</h2>
               </div>
-              <h2 className="">Ofline</h2>
-            </div>
+            </Link>
           </div>
-        </Link>
-      </div>
-      <div className="mt-1">
-        <Link href="/chat">
-          <div className="border-2 p-1 rounded-lg">
-            <div className="flex justify-between items-center">
-              <div className="flex items-center gap-2">
-                <div className="size-10">
-                  <Image
-                    // src={selectedImg || authUser.profilePic || "/avatar.png"}
-                    src={userProfile}
-                    alt="User image"
-                    className="size-full object-cover"
-                  />
-                </div>
-                <h2 className="font-semibold">Omar Faruk</h2>
-              </div>
-              <h2 className="">Ofline</h2>
-            </div>
-          </div>
-        </Link>
-      </div>
+        ))
+      ) : (
+        <div>Contacts list is Empty</div>
+      )}
     </div>
   );
 };
